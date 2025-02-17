dababc4d0d40894d00971f559ffca465456caa54
eskimopie910 <mitch91099@gmail.com>
2025-02-17 13:15:02 -0600

    move DB selector into DB dir && add search button back to animalSearch

--- src/database/dbConnection.ts
+++ src/database/dbConnection.ts
@@ -1,3 +1,4 @@
+import { dialog } from "electron";
 import pkg from 'sqlite3';
 
 // Destructure the Database class as a value
@@ -17,4 +18,22 @@ export const openDb = async (dbPath: string): Promise<InstanceType<typeof Databa
 // Function to get the existing database instance
 export const getDatabase = (): InstanceType<typeof Database> | null => {
   return dbInstance;
+};
+
+export const selectNewDb = async () => {
+  const { filePaths } = await dialog.showOpenDialog({
+    title: "Select Database File",
+    properties: ["openFile"],
+    filters: [{ name: "SQLite Database", extensions: ["db", "sqlite"] }],
+  });
+
+  if (filePaths.length > 0) {
+    const databasePath = filePaths[0]; // Save the selected file path
+
+    openDb(databasePath); // Open the database in dbConnection
+
+    return databasePath;
+  }
+
+  return null; // No file selected
 };
--- src/main/ipcHandlers.ts
+++ src/main/ipcHandlers.ts
@@ -1,7 +1,7 @@
 
 import { ipcMain, dialog } from "electron";
 import { animalSearch } from "../database/queries/animalSearch.js";
-import { openDb } from "../database/dbConnection.js";
+import { selectNewDb } from "../database/dbConnection.js";
 
 export const registerIpcHandlers = () => {
 
@@ -10,22 +10,23 @@ export const registerIpcHandlers = () => {
   });
 
   // Handler for "select-database"
-  ipcMain.handle("select-database", async () => {
-    const { filePaths } = await dialog.showOpenDialog({
-      title: "Select Database File",
-      properties: ["openFile"],
-      filters: [{ name: "SQLite Database", extensions: ["db", "sqlite"] }],
-    });
+  ipcMain.handle("select-database", selectNewDb);
+  // ipcMain.handle("select-database", async () => {
+  //   const { filePaths } = await dialog.showOpenDialog({
+  //     title: "Select Database File",
+  //     properties: ["openFile"],
+  //     filters: [{ name: "SQLite Database", extensions: ["db", "sqlite"] }],
+  //   });
 
-    if (filePaths.length > 0) {
-      const databasePath = filePaths[0]; // Save the selected file path
+  //   if (filePaths.length > 0) {
+  //     const databasePath = filePaths[0]; // Save the selected file path
 
-      openDb(databasePath); // open database in dbConnection
+  //     openDb(databasePath); // open database in dbConnection
 
-      return databasePath;
-    }
+  //     return databasePath;
+  //   }
 
-    return null; // No file selected
-  });
+  //   return null; // No file selected
+  // });
 };
 
