e219cdeafdcee81186b6686d9f84646f4d1a80d7
eskimopie910 <mitch91099@gmail.com>
2025-03-26 15:02:45 -0500

    update getDefaultsQuery to use * instead of explicitly typing the columns

--- src/database/repositories/read/defaults/getExistingDefaults.ts
+++ src/database/repositories/read/defaults/getExistingDefaults.ts
@@ -10,95 +10,7 @@ export const getExistingDefaults = async (): Promise<DefaultSettingsResults[]> =
 
   let defaultsQuery = `
     SELECT 
-      id_animaltrakkerdefaultsettingsid AS id, 
-      default_settings_name AS name,
-
-      owner_id_contactid,
-      owner_id_companyid,
-      owner_id_premiseid,
-      breeder_id_contactid,
-      breeder_id_companyid,
-      breeder_id_premiseid,
-      vet_id_contactid,
-      vet_id_premiseid,
-      lab_id_companyid,
-      lab_id_premiseid,
-      id_registry_id_companyid,
-      registry_id_premiseid,
-      id_stateid,
-      id_countyid,
-      id_flockprefixid,
-      id_speciesid,
-      id_breedid,
-      id_sexid,
-      id_idtypeid_primary,
-      id_idtypeid_secondary,
-      id_idtypeid_tertiary,
-      
-      id_eid_tag_male_color_female_color_same,
-      eid_tag_color_male,
-      eid_tag_color_female,
-      eid_tag_location,
-      id_farm_tag_male_color_female_color_same,
-      farm_tag_based_on_eid_tag,
-      farm_tag_number_digits_from_eid,
-      farm_tag_color_male,
-      farm_tag_color_female,
-      farm_tag_location,
-      id_fed_tag_male_color_female_color_same,
-      fed_tag_color_male,
-      fed_tag_color_female,
-      fed_tag_location,
-      id_nues_tag_male_color_female_color_same,
-      nues_tag_color_male,
-      nues_tag_color_female,
-      nues_tag_location,
-      id_trich_tag_male_color_female_color_same,
-      trich_tag_color_male,
-      trich_tag_color_female,
-      trich_tag_location,
-      trich_tag_auto_increment,
-      trich_tag_next_tag_number,
-      
-      id_bangs_tag_male_color_female_color_same,
-      bangs_tag_color_male,
-      bangs_tag_color_female,
-      bangs_tag_location,
-      id_sale_order_tag_male_color_female_color_same,
-      sale_order_tag_color_male,
-      sale_order_tag_color_female,
-      sale_order_tag_location,
-      
-      use_paint_marks,
-      paint_mark_color,
-      paint_mark_location,
-      tattoo_color,
-      tattoo_location,
-      freeze_brand_location,
-      
-      id_idremovereasonid,
-      id_tissuesampletypeid,
-      id_tissuetestid,
-      id_tissuesamplecontainertypeid,
-      
-      birth_type,
-      rear_type,
-      minimum_birth_weight,
-      maximum_birth_weight,
-      birth_weight_id_unitsid,
-      weight_id_unitsid,
-      sale_price_id_unitsid,
-      
-      evaluation_update_alert,
-      
-      death_reason_id_contactid,
-      death_reason_id_companyid,
-      id_deathreasonid,
-      
-      transfer_reason_id_contactid,
-      transfer_reason_id_companyid,
-      id_transferreasonid
-
+      *
     FROM animaltrakker_default_settings_table`;
 
   return new Promise((resolve, reject) => {
@@ -121,8 +33,8 @@ export const getExistingDefaults = async (): Promise<DefaultSettingsResults[]> =
           }
 
           return {
-            id: row.id,
-            name: row.name,
+            id: row.id_animaltrakkerdefaultsettingsid,
+            name: row.default_settings_name,
             owner_id: owner_id,
             owner_type: owner_type,
             owner_id_premiseid: String(row.owner_id_premiseid),
