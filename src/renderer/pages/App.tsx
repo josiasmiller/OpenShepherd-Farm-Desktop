1d2a2cf56da6a416b4b15fdfbf7c392b817bb21a
eskimopie910 <mitch91099@gmail.com>
2025-05-08 12:52:06 -0500

    fix: updates to fix prod loading errors

--- src/renderer/pages/App.tsx
+++ src/renderer/pages/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { HashRouter  as Router, Routes, Route } from "react-router-dom";
 import Home from "./common/home"
 import AnimalSearch from "./common/animalSearch/animalSearch"
 import CreateDefault from "./common/animalDefaults/createDefault"
