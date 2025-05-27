d7b33d045fa75a50269132f89e6d2c9c771d496a
eskimopie910 <mitch91099@gmail.com>
2025-05-27 14:34:07 -0500

    chore: more identifiers added to config

--- forge.config.cjs
+++ forge.config.cjs
@@ -10,7 +10,12 @@ const iconPath = path.resolve(__dirname, 'src/assets/icon.icns');
 /** @type {import('electron-forge').ForgeConfig} */
 module.exports = {
   packagerConfig: {
-    icon: path.resolve(__dirname, 'src/assets/icon')
+    icon: path.resolve(__dirname, 'src/assets/icon'),
+    name: 'AnimalTrakker Farm Desktop',
+    executableName: 'AnimalTrakker',
+    appBundleId: 'com.animaltrakker.farmdesktop',
+    appCategoryType: 'public.app-category.utilities', // mac specific categorization
+    productName: 'AnimalTrakker Farm Desktop',
   },
   makers: [
     new MakerZIP({}),
@@ -24,32 +29,3 @@ module.exports = {
     }, ['linux']),
   ],
 };
-
-// module.exports = {
-//   packagerConfig: {
-//     icon: iconPath.replace(/\.icns$/, ''), // must NOT include extension
-//     afterCopy: [
-//       async (buildPath) => {
-//         console.log('Starting afterCopy hook...');
-//         if (process.platform === 'darwin' && fs.existsSync(iconPath)) {
-//           const dest = path.join(buildPath, 'Contents', 'Resources', 'icon.icns');
-//           await fsp.copyFile(iconPath, dest);
-//           console.log('✅ Copied icon.icns to', dest);
-//         }
-//         console.log('afterCopy hook finished');
-//       }
-//     ]
-//   },
-//   makers: [
-//     new MakerZIP({}),
-//     new MakerDMG({
-//       icon: iconPath, // This sets the icon in the DMG window
-//     }, ['darwin']),
-//     new MakerDeb({
-//       options: {
-//         icon: path.resolve(__dirname, 'src/assets/AnimalTrakker_icon_512x512.png')
-//       }
-//     }, ['linux']),
-//   ],
-// };
-
