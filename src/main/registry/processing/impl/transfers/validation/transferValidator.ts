4b4138ad2340432b46719711cbfe11efb98c59e0
eskimopie910 <mitch91099@gmail.com>
2025-09-01 12:10:18 -0500

    fix: remove poor coat color check

--- src/main/registry/processing/impl/transfers/validation/transferValidator.ts
+++ src/main/registry/processing/impl/transfers/validation/transferValidator.ts
@@ -1,11 +1,6 @@
 import type { PedigreeNode, Species, RegistryRow, ValidationResult } from 'packages/api';
 import { checkHasFederalId } from './rules/checkHasFederalId';
 
-import { getPedigree } from '../../../../../database';
-import { verifyCoatColorAccuracy } from './rules/coatColorSanityCheck';
-
-import { handleResult } from "packages/core";
-
 
 /**
  * validates the data extracted from a registration CSV
@@ -22,33 +17,9 @@ export async function validateTransferRows(sections: Record<string, RegistryRow[
     const row = rows[index];
     const errors: string[] = [];
 
-    const animalId : string = row.animalId;
-    let pedigreeResult = await getPedigree(animalId, 1);
-    let baseNode : PedigreeNode
-    await handleResult(pedigreeResult, {
-      success: (pn: PedigreeNode) => {
-        baseNode = pn;
-      },
-      error: (err) => {
-        throw new Error(`Failed to get pedigreeNode for animalId=${animalId}: ${err}`);
-      },
-    });
-
-    baseNode = baseNode!;
-    
-    const sireId : string = baseNode.sirePedigree.animalId;
-    const damId : string = baseNode.damPedigree.animalId;
-
     // Run all relevant checks
     const federalCheck = await checkHasFederalId(row);
     errors.push(...federalCheck.errors);
-
-    const coatColorCheck = await verifyCoatColorAccuracy(
-      sireId,
-      damId,
-      animalId,
-    );
-    errors.push(...coatColorCheck.errors);
 
     results.push({
       rowIndex: index,
